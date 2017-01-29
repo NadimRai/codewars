382169fe841c0d43fc804d2378a0681fb5d2f6e2
NadimRai <rai_nadim@yahoo.com>
2017-01-29 03:50:34 -0800

    Basic Algorithm

--- 6kyu/ArrayDiff.js
+++ 6kyu/ArrayDiff.js
@@ -5,9 +5,29 @@ difference([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 difference([1,2,2,2,3],[2]) == [1,3]
 
-solution:
+solution 1:
 function array_diff(a, b) {
   return a.filter(function(value){
         return b.indexOf(value) == -1;
     });
+}
+
+solution 2:
+function array_diff(a, b) {
+	var arr = new Array();
+    for(var i = 0;i<a.length;i++){
+        if(b.indexOf(a[i])<0){
+            arr.push(a[i]);
+        }
+    }
+  
+    return arr;
+}
+
+solution 3:
+function array_diff(a, b) {
+  return a.filter(function(value)
+  {
+    return !(b.includes(value))  
+  });
 }
---
+++ gp
@@ -0,0 +1,3 @@
+git add .
+git commit -m"Basic Algorithm"
+git push origin master
