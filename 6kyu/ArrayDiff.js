a287570d257314822e67399592ac219395154e3a
NadimRai <rai_nadim@yahoo.com>
2017-01-30 04:04:45 -0800

    Codewars Javascript Solution

--- 6kyu/ArrayDiff.js
+++ 6kyu/ArrayDiff.js
@@ -1,3 +1,5 @@
+Array Difference 
+
 Your goal in this kata is to implement an difference function, which subtracts one list from another.
 It should remove all values from list a, which are present in list b.
 difference([1,2],[1]) == [2]
