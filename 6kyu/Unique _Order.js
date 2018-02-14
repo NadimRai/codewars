2f3586ba509220e95e9e57fa328c9657242fae19
NadimRai <rai_nadim@yahoo.com>
2018-02-14 13:54:52 +0800

    Unique order

--- 6kyu/Unique _Order.js
+++ 6kyu/Unique _Order.js
@@ -1,4 +1,4 @@
-Implement the function unique_in_order which takes as argument a sequence and returns a 
+/*Implement the function unique_in_order which takes as argument a sequence and returns a 
 list of items without any elements with the same value next to each other and preserving 
 the original order of elements.
 
@@ -6,6 +6,7 @@ Example:
 uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
 
 solution 1:
 var uniqueInOrder=function(iterable){
@@ -26,4 +27,18 @@ var uniqueInOrder = function (iterable){
 solution 3:
 var uniqueInOrder=function(iterable){
   return Array.prototype.filter.call(iterable, function(current, index){ return iterable[index - 1] !== current})
+}
+
+solution4:
+
+var uniqueInOrder=function(iterable){
+  var ansArray = [];
+  for(var i=0; i<iterable.length; i++){
+    var curr = iterable[i];
+    var next = iterable[i+1];
+    if(curr !== next){
+      ansArray.push(curr);
+    }
+  }
+  return ansArray;
 }
